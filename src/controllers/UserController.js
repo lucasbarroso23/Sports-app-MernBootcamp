f7922d50c9256b7b6cd5b9aa5bedb839b70d2c3c
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-05 17:12:56 -0300

    Finalização da primeira aula, salvando usuario e hasheando a senha

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,32 @@
 const User = require('../models/User');
-const { request } = require('express');
+const bcrypt = require('bcrypt');
+
 
 module.exports = {
     async store(req, res) {
         try {
-            console.log(req.body)
-            const {firstName, lastName, password, email} =  request.body;
+            const {firstName, lastName, password, email} =  req.body;
+
+            const existentUser = await User.findOne({email});
+
+            if(!existentUser) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = await User.create({
+                    firstName,
+                    lastName,
+                    email,
+                    password: hashedPassword
+                });
+
+                return res.json(user)
+            }
+
+            return res.status(400).json({
+                message:'email already exist! Do you want to login instead?'
+            });
+
         } catch (error) {
-            
+            throw Error(`Error while registering a new user : ${error}`)
         }
     }
 }
