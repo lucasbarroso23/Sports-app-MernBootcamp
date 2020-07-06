3a434e6d8cc27ee7bdd61971ea75abc635e66007
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-06 12:18:23 -0300

    finalizando a segunda aula foi feito o metodo getEventById

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -28,6 +28,27 @@ module.exports = {
 
 
         return res.json(event);
+    }, 
+
+    async getEventById(req, res) {
+
+        const { eventId} =  req.params;
+
+        try {
+            const event = await Event.findById(eventId)
+
+            if(event) {
+                return res.json(event);
+            }
+
+        } catch (error) {
+            
+            return res.status(400).json({ message: "Event ID does not exist!" })
+        }
+
+
+        
+
     }
 
 
