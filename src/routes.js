1a0792b7d2ab133827937c82385bd39f873eddd6
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-06 11:57:07 -0300

    model e controller de evento criados

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const routes = express.Router();
+const multer = require('multer');
+
+const uploadConfig = require('./config/upload');
 const UserController = require('./controllers/UserController');
+const EventController = require('./controllers/EventController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.get('/status', (req, res) => { 
     res.send({ status: 200 });
 })
 
+//Event
+routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
+
+
+//User
 routes.post('/user/register', UserController.createUser)
 
 routes.get('/user/:userId', UserController.getUserById)
