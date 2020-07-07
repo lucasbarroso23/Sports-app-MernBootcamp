665accb5f3ec823f2890df03c84e19007531abe8
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-07 11:22:29 -0300

    adicionado os métodos para selecionar todos os eventos e selescionar todos os eventos por esporte

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -1,4 +1,4 @@
-const Event = require('../models/Events');
+const Event = require('../models/Event');
 const User = require('../models/User');
 
 
@@ -7,7 +7,7 @@ module.exports = {
 
     async createEvent (req, res) {
 
-        const { title, description, price } = req.body; 
+        const { title, description, price, sport } = req.body; 
         const { user_id } = req.headers;
         const { filename } = req.file;
 
@@ -20,6 +20,7 @@ module.exports = {
         const event = await Event.create({
             title,
             description,
+            sport,
             price: parseFloat(price),
             user: user_id,
             thumbnail: filename
@@ -49,7 +50,50 @@ module.exports = {
 
         
 
+    }, 
+
+    async getAllEvents(req, res) {
+
+        try {
+            const events = await Event.find({})
+
+            if(events) {
+                return res.json(events);
+            }
+
+        } catch (error) {
+            
+            return res.status(400).json({ message: "Sorry, we dont have any events yet" })
+        }
+
+
+        
+
+    }, 
+
+    async getAllEventsBySport(req, res) {
+
+        const { sport } = req.params;
+        const query = { sport } || {}
+
+        try {
+            const events = await Event.find(query)
+
+            if(events) {
+                return res.json(events);
+            }
+
+        } catch (error) {
+            
+            return res.status(400).json({ message: "Sorry, we dont have any events yet" })
+        }
+
+
+        
+
     }
+
+
 
 
 }
--- src/routes.js
+++ src/routes.js
@@ -12,14 +12,15 @@ routes.get('/status', (req, res) => {
     res.send({ status: 200 });
 })
 
-//Event
+//Events
+routes.get('/events', EventController.getAllEvents)
+routes.get('/events/:sport', EventController.getAllEventsBySport)
 routes.get('/event/:eventId', EventController.getEventById)
 routes.post('/event', upload.single("thumbnail"), EventController.createEvent)
 
 
 //User
 routes.post('/user/register', UserController.createUser)
-
 routes.get('/user/:userId', UserController.getUserById)
 
 module.exports = routes;
