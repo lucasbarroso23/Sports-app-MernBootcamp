882e9fdc1bd07b61294c6629a73f0bbe4829d13a
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-17 11:27:24 -0300

    Desenvolvendo o dashboard com os eventos

--- backend/src/controllers/EventController.js
+++ backend/src/controllers/EventController.js
@@ -8,7 +8,7 @@ module.exports = {
 
     async createEvent (req, res) {
 
-        const { title, description, price, sport } = req.body; 
+        const { title, description, price, sport, date } = req.body; 
         const { user_id } = req.headers;
         const { filename } = req.file;
 
@@ -24,7 +24,8 @@ module.exports = {
             sport,
             price: parseFloat(price),
             user: user_id,
-            thumbnail: filename
+            thumbnail: filename,
+            date
 
         })
 
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import api from "../../services/api";
+import moment from 'moment';
+import './dashboard.css';
 //Dashboard will show all the events
 export default function Dashboard(){
-
+    const [events, setEvents] = useState([]);
     const user_id = localStorage.getItem('user');
 
-    console.log(user_id)
+    useEffect(() => {
+        getEvents()
+    }, [])
 
+    const getEvents = async (filter) => {
+        const url = filter ? `/dashboard/${filter}` : '/dashboard'
+        const response = await api.get(url, {headers: { user_id }});
+
+        setEvents(response.data)
+    };
+
+    console.log(events)
     return (
-        <div>
-            Hello from Dashboard
-        </div>
+        <ul className="events-list">
+            {events.map(event => (
+                <li key={event._id}>
+                    <header style={{ backgroundImage: `url(${event.thumbnail_url})`}} />
+                    <strong>{event.title}</strong>
+                    <span>{moment(event.date).format('DD/MM/YYYY')}</span>
+                    <span>{event.price}</span>
+                    <span>{event.description}</span>
+
+                </li>
+            ))}
+        </ul>
     )
 }
